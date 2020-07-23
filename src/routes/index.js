768c756b5f091db678ada772c75692b512c76362
Patrick Lima <patrickhl94@hotmail.com>
2020-07-23 01:15:46 -0300

    Created index for routes

---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Home from '../Pages/Home';
+import ListTasks from '../Pages/ListTasks';
+import Register from '../Pages/Register';
+import Login from '../Pages/Login';
+
+const Routes = () => (
+  <Switch>
+    <Route path="/" exact component={Home} />
+    <Route path="/list" component={ListTasks} />
+    <Route path="/register" component={Register} />
+    <Route path="/login" component={Login} />
+  </Switch>
+);
+
+export default Routes;
