e9902ab6e27571ba68f273ea07f60114d462f732
Patrick Lima <patrickhl94@hotmail.com>
2020-07-23 01:10:58 -0300

    Init and config eslint

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2020": true
+    },
+    "extends": [
+        "plugin:react/recommended",
+        "airbnb",
+        'prettier',
+        'prettier/react'
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 11,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "react-hooks",
+        "prettier"
+    ],
+    "rules": {
+        'prettier/prettier': 'error',
+        "react-hooks/rules-of-hooks": "error",
+        "react-hooks/exhaustive-deps": "warn",
+        'react/jsx-filename-extension': [
+            'warn',
+            { extensions: ['.jsx', '.js'] }
+        ],
+        'import/prefer-default-export': 'off',
+        'class-methods-use-this': 'off',
+        'no-param-reassign': 'off',
+        'react/prefer-stateless-function': 'off',
+        'react/no-unused-state': 'off',
+        'react/state-in-constructor': 'off',
+        'react/destructuring-assignment': 'off',
+        'camelcase': 'off',
+        'no-unused-vars': ['error', { 'argdIgnorePAtter': 'next' }],
+        'linebreak-style': 0,
+        'global-require': 0,
+        'react/static-property-placement': 'off'
+    }
+};
