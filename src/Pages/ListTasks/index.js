7eb90a95ccd6dc30098475f8277f5329d3684d21
Patrick Lima <patrickhl94@hotmail.com>
2020-07-23 01:15:12 -0300

    Created pages

---
+++ src/Pages/ListTasks/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ListTasks = () => {
+  return <h1>ListTasks</h1>;
+};
+
+export default ListTasks;
---
+++ src/Pages/Register/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Register = () => {
+  return (
+    <Container>
+      <h1>Register</h1>
+    </Container>
+  );
+};
+
+export default Register;
