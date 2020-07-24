2297829e71e80b4e2133ed3afeab0e5542b63866
Patrick Lima <patrickhl94@hotmail.com>
2020-07-23 22:14:13 -0300

    Updated index pages Login and Register

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,11 +1,45 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-import { Container } from './styles';
+import {
+  Container,
+  LeftCard,
+  RightCard,
+  LoginCard,
+  AreaRegister,
+} from './styles';
+import Input from '../../components/InputLogin';
+
+import logo from '../../assets/images/logo.svg';
 
 const Login = () => {
+  const userReducer = useSelector(state => state.user);
+  console.log('INITIAL ', userReducer);
+
   return (
     <Container>
-      <h1>Login</h1>
+      <LoginCard>
+        <LeftCard>
+          <img src={logo} width="300px" alt="Logo ListTask" />
+          <h1>ListTasks</h1>
+        </LeftCard>
+
+        <RightCard>
+          <h1>Entrar no ListTasks</h1>
+          <form onSubmit={() => alert('Foi')}>
+            <Input data={{ type: 'text', name: 'E-mail' }} />
+            <Input data={{ type: 'password', name: 'Password' }} />
+
+            <button type="submit">Entrar</button>
+          </form>
+
+          <AreaRegister>
+            <p>É novo por aqui?</p>
+            <Link to="/register">Cadastre-se</Link>
+          </AreaRegister>
+        </RightCard>
+      </LoginCard>
     </Container>
   );
 };
--- src/Pages/Register/index.js
+++ src/Pages/Register/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
-import { Container } from './styles';
+import Routes from './routes';
+
+import { Container, LeftCard, RightCard, LoginCard } from './styles';
+
+import logo from '../../assets/images/logo.svg';
 
 const Register = () => {
   return (
     <Container>
-      <h1>Register</h1>
+      <LoginCard>
+        <LeftCard>
+          <img src={logo} width="300px" alt="Logo ListTask" />
+          <h1>ListTasks</h1>
+        </LeftCard>
+
+        <RightCard>
+          <h1>Cadastre-se no ListTasks</h1>
+          <Routes />
+        </RightCard>
+      </LoginCard>
     </Container>
   );
 };
