38d70c5c189ac14d94281edd4b7abb4dc8c4c8b2
Patrick Lima <patrickhl94@hotmail.com>
2020-07-24 23:55:47 -0300

    Updated style page Register

--- src/Pages/Register/Pages/styles.js
+++ src/Pages/Register/Pages/styles.js
@@ -1,23 +1,13 @@
 import styled from 'styled-components';
 
-export const AreaProxOrRetu = styled.div`
-  margin-top: 15px;
-  width: 100%;
-  display: flex;
-
-  a {
-    display: flex;
-    margin-left: auto;
-    margin-right: auto;
-    align-items: center;
-    text-decoration: none;
-    font-weight: 700;
-    color: #50758d;
+export const Container = styled.div`
+  small {
+    color: red;
+    font-weight: 500;
   }
 `;
 
 export const ButtonRegister = styled.button`
-  text-decoration: none;
   margin-left: 8px;
   height: 35px;
   padding: 8px 12px;
@@ -32,5 +22,48 @@ export const ButtonRegister = styled.button`
   &:hover {
     color: #2b4656;
     border: solid 1.5px #eac007;
+  }
+`;
+
+export const AreaProxOrRetu = styled.div`
+  margin-top: 15px;
+  width: 100%;
+  display: flex;
+
+  > button {
+    opacity: ${({ disabledButton }) => (disabledButton ? 1 : 0.5)};
+    cursor: ${({ disabledButton }) =>
+      disabledButton ? 'pointer' : 'not-allowed'};
+    background: 0;
+    border: 0;
+    display: flex;
+    margin-left: auto;
+    margin-right: auto;
+    align-items: center;
+    text-decoration: none;
+    font-weight: 700;
+    color: #50758d;
+    transition: color 0.3s;
+
+    &:hover {
+      color: #2b4656;
+    }
+  }
+
+  a {
+    background: 0;
+    border: 0;
+    display: flex;
+    margin-left: auto;
+    margin-right: auto;
+    align-items: center;
+    text-decoration: none;
+    font-weight: 700;
+    color: #50758d;
+    transition: color 0.3s;
+
+    &:hover {
+      color: #2b4656;
+    }
   }
 `;
