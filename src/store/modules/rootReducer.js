03344af29d547b0078da755e2e1b821abc78e099
Patrick Lima <patrickhl94@hotmail.com>
2020-07-24 23:56:45 -0300

    Updated module redux rootReducer

--- src/store/modules/rootReducer.js
+++ src/store/modules/rootReducer.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 
-import user from './user/reducer';
+import userRegister from './userRegister/reducer';
 
 export default combineReducers({
-  user,
+  userRegister,
 });
