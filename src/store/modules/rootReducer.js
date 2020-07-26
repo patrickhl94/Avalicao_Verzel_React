c76590568153c70ad25001b463ba19f46945fa39
Patrick Lima <patrickhl94@hotmail.com>
2020-07-26 19:05:18 -0300

    Insert in root reducer listTask

--- src/store/modules/rootReducer.js
+++ src/store/modules/rootReducer.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 
 import userRegister from './userRegister/reducer';
 import session from './session/reducer';
+import listTask from './listTask/reducer';
 
 export default combineReducers({
   userRegister,
   session,
+  listTask,
 });
