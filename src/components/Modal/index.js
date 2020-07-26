f82d85740b5e4e4bc3158248f956c2575bc94ff8
Patrick Lima <patrickhl94@hotmail.com>
2020-07-26 19:04:31 -0300

    Updated component Modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,4 +1,8 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { uuid } from 'uuidv4';
+import * as Yup from 'yup';
+import { useDispatch, useSelector, shallowEqual } from 'react-redux';
 import { FiXCircle } from 'react-icons/fi';
 
 import {
@@ -9,41 +13,138 @@ import {
 } from './styles';
 import { Input, TextArea } from '../Input';
 
-const Modal = ({ modalOpen, dataProp }) => {
+import logo from '../../assets/images/logo.svg';
+
+const Modal = ({ dataPropModal }) => {
   const [isOpenState, setIsOpenState] = useState(true);
   const [registerTask, setRegisterTask] = useState({});
+  const [smallMsg, setSmallMsg] = useState(
+    'Deixe a data de conclusão em branco, caso não tenha previsão de conclusão',
+  );
+  const [smallError, setSmallError] = useState(false);
+  const dispath = useDispatch();
+  const taskUpdate = useSelector(state =>
+    state.listTask.filter(
+      tasks => tasks.id === dataPropModal.idTask,
+      shallowEqual,
+    ),
+  );
+
+  useEffect(() => {
+    const task = taskUpdate[0];
+    if (task && dataPropModal.idTask) {
+      const { dateConclusion, dateDelivery, description, name } = task;
+      setRegisterTask({ dateConclusion, dateDelivery, description, name });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [dataPropModal.idTask]);
 
   const closeModal = useCallback(() => {
-    if (modalOpen) return setIsOpenState(false);
+    dataPropModal.idTask = null;
+    setRegisterTask({});
+
+    if (dataPropModal.modalOpen) return setIsOpenState(false);
     return setIsOpenState(true);
-  }, [modalOpen]);
+  }, [dataPropModal]);
 
   const onChange = useCallback(
     (value, nameValue) => {
       registerTask[nameValue] = value;
-
       setRegisterTask({ ...registerTask });
     },
     [registerTask],
   );
 
-  const registerNewTaks = useCallback(() => {
-    console.log(registerTask);
-  }, [registerTask]);
+  const registerNewTaks = useCallback(async () => {
+    const schema = Yup.object().shape({
+      name: Yup.string().required('O nome da tarefa é obrigatório'),
+      dateDelivery: Yup.string().required('A data da tarefa é obrigatória'),
+      dateConclusion: Yup.string(),
+      description: Yup.string().required('A descrição da tarefa é obrigatória'),
+    });
+
+    setSmallMsg(
+      'Deixe a data de conclusão em branco, caso não tenha previsão de conclusão',
+    );
+    setSmallError(false);
+
+    try {
+      await schema.validate(registerTask);
+
+      dispath({
+        type: 'ADD_TASK',
+        task: {
+          ...registerTask,
+          id: uuid(),
+          status: false,
+          toViewTask: false,
+        },
+      });
+
+      closeModal();
+    } catch (error) {
+      setSmallMsg(error.message);
+      setSmallError(true);
+    }
+  }, [registerTask, dispath, closeModal]);
+
+  const updateTaks = useCallback(async () => {
+    const schema = Yup.object().shape({
+      name: Yup.string().required('O nome da tarefa é obrigatório'),
+      dateDelivery: Yup.string().required('A data da tarefa é obrigatória'),
+      dateConclusion: Yup.string(),
+      description: Yup.string().required('A descrição da tarefa é obrigatória'),
+    });
+
+    setSmallMsg(
+      'Deixe a data de conclusão em branco, caso não tenha previsão de conclusão',
+    );
+    setSmallError(false);
+
+    const { dateConclusion, dateDelivery, description, name } = registerTask;
+
+    try {
+      await schema.validate(registerTask);
+      dispath({
+        type: 'UPTADE_TASK',
+        task: {
+          dateConclusion: dateConclusion || '',
+          dateDelivery,
+          description,
+          name,
+        },
+        id: dataPropModal.idTask,
+      });
+
+      closeModal();
+    } catch (error) {
+      setSmallMsg(error.message);
+      setSmallError(true);
+    }
+  }, [registerTask, dispath, closeModal, dataPropModal.idTask]);
 
   return (
-    <Container isOpenState={isOpenState} isOpenProp={modalOpen}>
-      <ModalBody>
+    <Container isOpenState={isOpenState} isOpenProp={dataPropModal.modalOpen}>
+      <ModalBody colorSmall={smallError}>
         <ModalHeader>
           <FiXCircle onClick={closeModal} size={30} color="#50758d" />
         </ModalHeader>
+
+        <div>
+          <h1>
+            {' '}
+            {dataPropModal.isRegister ? 'Cadastre uma ' : 'Atualizar '}
+            tarefa
+          </h1>
+          <img src={logo} alt="Logo ListTasks" width="40px" />
+        </div>
 
         <Input
           data={{
             onChange,
             value: registerTask.name,
             type: 'text',
-            label: 'Nome',
+            label: 'Nome *',
             name: 'name',
           }}
         />
@@ -52,7 +153,7 @@ const Modal = ({ modalOpen, dataProp }) => {
             onChange,
             value: registerTask.dateDelivery,
             type: 'date',
-            label: 'Data de entrega',
+            label: 'Data de entrega *',
             name: 'dateDelivery',
           }}
         />
@@ -71,23 +172,38 @@ const Modal = ({ modalOpen, dataProp }) => {
             onChange,
             value: registerTask.description,
             type: 'text',
-            label: 'Descrição',
+            label: 'Descrição *',
             name: 'description',
           }}
         />
-        <small style={{ color: 'blue' }}>
-          Deixe a data de conclusão em branco, caso não tenha previsão de
-          conclusão
-        </small>
+
+        <small>{smallMsg}</small>
 
         <AreaButtonRegister>
-          <button onClick={registerNewTaks} type="button">
-            Cadastrar
-          </button>
+          {dataPropModal.isRegister ? (
+            <button onClick={registerNewTaks} type="button">
+              Cadastrar
+            </button>
+          ) : (
+            <button
+              onClick={() => updateTaks(dataPropModal.idTask)}
+              type="button"
+            >
+              Atualizar
+            </button>
+          )}
         </AreaButtonRegister>
       </ModalBody>
     </Container>
   );
+};
+
+Modal.propTypes = {
+  dataPropModal: PropTypes.shape({
+    isRegister: PropTypes.bool,
+    idTask: PropTypes.string,
+    modalOpen: PropTypes.bool,
+  }).isRequired,
 };
 
 export default Modal;
