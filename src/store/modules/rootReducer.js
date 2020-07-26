5eed4afbe1a4863521109f104ad9cb31a67f96e9
Patrick Lima <patrickhl94@hotmail.com>
2020-07-25 22:31:36 -0300

    Insert session in rootReducer

--- src/store/modules/rootReducer.js
+++ src/store/modules/rootReducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import userRegister from './userRegister/reducer';
+import session from './session/reducer';
 
 export default combineReducers({
   userRegister,
+  session,
 });
