e9aa79387e3f438de0343c5e869a9945571efb10
Patrick Lima <patrickhl94@hotmail.com>
2020-07-26 23:52:27 -0300

    Updated store reducer session

--- src/store/modules/session/reducer.js
+++ src/store/modules/session/reducer.js
@@ -4,6 +4,14 @@ function session(state = {}, action) {
       return { ...state, email: action.email };
     case 'ADD_PASSWORD':
       return { ...state, password: action.password };
+    case 'ADD_TOKEN':
+      return { ...state, token: action.token };
+    case 'ADD_ID':
+      return { ...state, id: action.id };
+    case 'START_SESSION':
+      return action.data;
+    case 'END_SESSION':
+      return { ...state, token: '' };
     default:
       return state;
   }
