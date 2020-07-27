9488f069c616cda038d8671cb216fd224d239f23
Patrick Lima <patrickhl94@hotmail.com>
2020-07-26 23:49:49 -0300

    Updated page ListTasks

--- src/Pages/ListTasks/index.js
+++ src/Pages/ListTasks/index.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState, useEffect } from 'react';
+import React, { useCallback, useState /* useEffect */ } from 'react';
 import { useSelector, shallowEqual, useDispatch } from 'react-redux';
 import { format, parseISO } from 'date-fns';
 import {
@@ -20,7 +20,7 @@ import {
 } from './styles';
 import Modal from '../../components/Modal';
 
-import api from '../../services/api';
+// import api from '../../services/api';
 
 const ListTasks = () => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
@@ -28,6 +28,31 @@ const ListTasks = () => {
   const [idTaskUpdate, setIdTaskUpdate] = useState(null);
   const listTasksReducer = useSelector(state => state.listTask, shallowEqual);
   const dispatch = useDispatch();
+  // const session = useSelector(state => state.session, shallowEqual);
+
+  /**
+   *  useEffect QUE BUSCA OS DADOS NA API FAKE, BASTA
+   *  RETIRAR OS COMENTÁRIO DAS LINHAS, 1, 23, 31 E 42 AO 55
+   *
+   * NESTE useEffect, A APLICAÇÃO IRÁ REALIZAR A REQUISIÇÃO À API
+   * PARA BUSCAR A LISTAGEM DO BANCO DE DADOS, CASO ELA NÃO CONSIGA,
+   * À APLICAÇÃO IRÁ MOSTRAR A RELAÇÃO SALVA NO STORAGE PELO REDUX.
+   */
+
+  // useEffect(() => {
+  //   const { id } = session;
+  //   api
+  //     .get(`tasks?idUser=${id}`)
+  //     .then(response => {
+  //       dispatch({
+  //         type: 'UPDATE_ALL_TASKS',
+  //         newListTasks: response.data,
+  //       });
+  //     })
+  //     .catch(error => {
+  //       console.log(error);
+  //     });
+  // }, [dispatch, session]);
 
   const toViewTaskAction = useCallback(
     id => {
@@ -41,15 +66,13 @@ const ListTasks = () => {
 
   const removeTaskList = useCallback(
     async id => {
-      try {
-        // await api.delete(`/delete/${id}`);
-        dispatch({
-          type: 'DELETE_TASK',
-          id,
-        });
-      } catch (error) {
-        console.log(error);
+      if (!window.confirm('Confirma exclusão desta tarefa?')) {
+        return;
       }
+      dispatch({
+        type: 'DELETE_TASK',
+        id,
+      });
     },
     [dispatch],
   );
