79e440a0f96c0825a4b37231e8395c3a61d61309
Patrick Lima <patrickhl94@hotmail.com>
2020-07-28 21:25:43 -0300

    Updated page ListTasks

--- src/Pages/ListTasks/index.js
+++ src/Pages/ListTasks/index.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState /* useEffect */ } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useSelector, shallowEqual, useDispatch } from 'react-redux';
 import { format, parseISO } from 'date-fns';
 import {
@@ -20,39 +20,16 @@ import {
 } from './styles';
 import Modal from '../../components/Modal';
 
-// import api from '../../services/api';
-
 const ListTasks = () => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [modalIsRegister, setModalIsRegister] = useState(true);
   const [idTaskUpdate, setIdTaskUpdate] = useState(null);
-  const listTasksReducer = useSelector(state => state.listTask, shallowEqual);
+  const session = useSelector(state => state.session, shallowEqual);
+  const listTasksReducer = useSelector(
+    state => state.listTask.filter(tasks => tasks.userId === session.id),
+    shallowEqual,
+  );
   const dispatch = useDispatch();
-  // const session = useSelector(state => state.session, shallowEqual);
-
-  /**
-   *  useEffect QUE BUSCA OS DADOS NA API FAKE, BASTA
-   *  RETIRAR OS COMENTÁRIO DAS LINHAS, 1, 23, 31 E 42 AO 55
-   *
-   * NESTE useEffect, A APLICAÇÃO IRÁ REALIZAR A REQUISIÇÃO À API
-   * PARA BUSCAR A LISTAGEM DO BANCO DE DADOS, CASO ELA NÃO CONSIGA,
-   * À APLICAÇÃO IRÁ MOSTRAR A RELAÇÃO SALVA NO STORAGE PELO REDUX.
-   */
-
-  // useEffect(() => {
-  //   const { id } = session;
-  //   api
-  //     .get(`tasks?idUser=${id}`)
-  //     .then(response => {
-  //       dispatch({
-  //         type: 'UPDATE_ALL_TASKS',
-  //         newListTasks: response.data,
-  //       });
-  //     })
-  //     .catch(error => {
-  //       console.log(error);
-  //     });
-  // }, [dispatch, session]);
 
   const toViewTaskAction = useCallback(
     id => {
@@ -128,71 +105,75 @@ const ListTasks = () => {
         </AreaButonGegister>
 
         <ul>
-          {listTasksReducer.map(task => (
-            <ListItem key={task.id}>
-              <div>
+          {listTasksReducer &&
+            listTasksReducer.map(task => (
+              <ListItem key={task.id}>
                 <div>
-                  <Button
-                    onClick={() => confimeTask(task.id, task.status)}
-                    type="button"
-                  >
-                    {task.status ? (
-                      <FaCheck size={25} color="#43db3b" />
-                    ) : (
-                      <FaRegCircle size={25} color="red" />
-                    )}
-                  </Button>
-
-                  <h2>{task.name}</h2>
-                </div>
-
-                <div>
-                  <Button onClick={() => removeTaskList(task.id)} type="button">
-                    <FaTrash size={25} color="#50758d" />
-                  </Button>
-
-                  <Button
-                    onClick={() => toViewTaskAction(task.id)}
-                    type="button"
-                  >
-                    <FaEye size={25} color="#50758d" />
-                  </Button>
-
-                  <Button
-                    onClick={() => openModalUpdate(task.id)}
-                    type="button"
-                  >
-                    <FaEdit size={25} color="#50758d" />
-                  </Button>
-                </div>
-              </div>
-
-              {task.toViewTask && (
-                <AreaVisualizationTask>
                   <div>
-                    <p>{task.description}</p>
+                    <Button
+                      onClick={() => confimeTask(task.id, task.status)}
+                      type="button"
+                    >
+                      {task.status ? (
+                        <FaCheck size={25} color="#43db3b" />
+                      ) : (
+                        <FaRegCircle size={25} color="red" />
+                      )}
+                    </Button>
+
+                    <h2>{task.name}</h2>
                   </div>
 
                   <div>
-                    <span>
-                      <strong>Status: </strong>{' '}
-                      {task.status ? 'Concluída' : 'Em aberto'}
-                    </span>
-                    <span>
-                      <strong>Data da entrega: </strong>{' '}
-                      {format(parseISO(task.dateDelivery), 'dd/MM/yyyy')}
-                    </span>
-                    <span>
-                      <strong>Data da conclusão: </strong>
-                      {task.dateConclusion
-                        ? format(parseISO(task.dateConclusion), 'dd/MM/yyyy')
-                        : 'Aguardando Conclusão'}
-                    </span>
+                    <Button
+                      onClick={() => removeTaskList(task.id)}
+                      type="button"
+                    >
+                      <FaTrash size={25} color="#50758d" />
+                    </Button>
+
+                    <Button
+                      onClick={() => toViewTaskAction(task.id)}
+                      type="button"
+                    >
+                      <FaEye size={25} color="#50758d" />
+                    </Button>
+
+                    <Button
+                      onClick={() => openModalUpdate(task.id)}
+                      type="button"
+                    >
+                      <FaEdit size={25} color="#50758d" />
+                    </Button>
                   </div>
-                </AreaVisualizationTask>
-              )}
-            </ListItem>
-          ))}
+                </div>
+
+                {task.toViewTask && (
+                  <AreaVisualizationTask>
+                    <div>
+                      <p>{task.description}</p>
+                    </div>
+
+                    <div>
+                      <span>
+                        <strong>Status: </strong>{' '}
+                        {task.status ? 'Concluída' : 'Em aberto'}
+                      </span>
+                      <span>
+                        <strong>Data da entrega: </strong>{' '}
+                        {format(parseISO(task.dateDelivery), 'dd/MM/yyyy')}
+                      </span>
+                      <span>
+                        <strong>Data da conclusão: </strong>
+                        {task.dateConclusion
+                          ? format(parseISO(task.dateConclusion), 'dd/MM/yyyy')
+                          : 'Aguardando Conclusão'}
+                      </span>
+                    </div>
+                  </AreaVisualizationTask>
+                )}
+              </ListItem>
+            ))}
         </ul>
       </ContainerList>
       <Modal
